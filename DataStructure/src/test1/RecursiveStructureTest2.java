e10528c304354f5ef1226688d7a8e4d3aba97da0
joowon <skcjs65dl@naver.com>
2024-02-04 18:03:53 +0900

    fibo commit!

---
+++ DataStructure/src/test1/RecursiveStructureTest2.java
@@ -0,0 +1,38 @@
+package test1;
+
+public class RecursiveStructureTest2 {
+
+	public static void main(String[] args) {
+		int first = 1;
+		int second = 1;
+		int third = 0;
+		
+		for (int i = 3; i <= 20; i++) {
+			third = first + second;
+			first = second;
+			second = third;
+		}
+		System.out.println(third);
+		
+		System.out.println(fibo(20));
+
+	}
+	
+	static int fibo(int n) {
+		if (n==1 || n==2) {
+			return 1;
+		}
+		int first = 1;
+		int second = 1;
+		int third = 0;
+		for (int i = 1; i <= n-2; i++) {
+			third = first + second;
+			first = second;
+			second = third;
+			
+		}
+		
+		return third;
+	}
+
+}
